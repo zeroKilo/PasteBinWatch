6256fc95259bd6b0ebac37ec9766c8f1edf89331
zeroKilo <b_d_b@web.de>
2015-10-23 18:31:44 +0200

    initial commit
    initial commit

---
+++ PasteBinWatch/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace PasteBinWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.wb1 = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtb1
+            // 
+            this.rtb1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtb1.Location = new System.Drawing.Point(0, 25);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(292, 248);
+            this.rtb1.TabIndex = 0;
+            this.rtb1.Text = "";
+            this.rtb1.WordWrap = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // wb1
+            // 
+            this.wb1.Location = new System.Drawing.Point(205, 178);
+            this.wb1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb1.Name = "wb1";
+            this.wb1.Size = new System.Drawing.Size(250, 250);
+            this.wb1.TabIndex = 1;
+            this.wb1.Visible = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(292, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.CheckOnClick = true;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(40, 22);
+            this.toolStripButton1.Text = "Pause";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.wb1);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "PasteBin Watcher";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.WebBrowser wb1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ PasteBinWatch/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasteBinWatch
+{
+    public partial class Form1 : Form
+    {
+        public string lastURL = "http://pastebin.com/";
+        public WebClient client = new WebClient();
+        public Form1()
+        {
+            InitializeComponent();
+            wb1.ScriptErrorsSuppressed = true;
+            wb1.Navigate(lastURL);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            wb1.Navigate(lastURL);
+            while (wb1.ReadyState != WebBrowserReadyState.Complete)
+                Application.DoEvents();
+            string html = wb1.DocumentText;
+            html = html.Substring(html.IndexOf("content_right_menu") + 18);
+            html = html.Substring(html.IndexOf("right_menu"));
+            html = html.Substring(html.IndexOf("a href=\"/") + 8);
+            html = html.Substring(0, html.IndexOf("\""));            
+            lastURL = "http://pastebin.com" + html;
+            this.Text = "PasteBin Watcher - " + lastURL;
+            HtmlElement el = wb1.Document.GetElementById("paste_code");
+            rtb1.Text = el.InnerText;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !toolStripButton1.Checked;
+        }
+    }
+}
